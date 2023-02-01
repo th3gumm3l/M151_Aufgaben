a5bda3ca486d69893fd3828d06e48add23875662
Tim Bernhard <tim.bernhard@me.com>
2023-02-01 15:52:36 +0100

    aufgabe1-3.php refactored

--- Aufgaben/Aufgabe 1/aufgabe1-3.php
+++ Aufgaben/Aufgabe 1/aufgabe1-3.php
@@ -1,12 +1,12 @@
 <?php
-    session_start();    //Neue Session wird gestartet
+session_start();    //Neue Session wird gestartet
 
-    $anzahl_aufrufe = 1;
-    if (isset($_SESSION['anzahl_aufrufe'])) {       //Prüfe ob es bereits eine gespeicherte anzahl aufrufe gibt
-        $anzahl_aufrufe = $_SESSION['anzahl_aufrufe'];
-    }
+$anzahl_aufrufe = 1;
+if (isset($_SESSION['anzahl_aufrufe'])) {       //Prüfe ob es bereits eine gespeicherte anzahl aufrufe gibt
+    $anzahl_aufrufe = $_SESSION['anzahl_aufrufe'];
+}
 
-    echo "Die Seite wurde {$anzahl_aufrufe}x aufgerufen.";
+echo "Die Seite wurde {$anzahl_aufrufe}x aufgerufen.";
 
-    $anzahl_aufrufe++;
-    $_SESSION['anzahl_aufrufe'] = $anzahl_aufrufe; //Muss wieder auf der Variable gespeichert werden
+$anzahl_aufrufe++;
+$_SESSION['anzahl_aufrufe'] = $anzahl_aufrufe; //Muss wieder auf der Variable gespeichert werden
