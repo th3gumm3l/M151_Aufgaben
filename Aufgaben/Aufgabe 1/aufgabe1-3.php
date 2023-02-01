04f530185a9c34eabeede365acfce409a66ccdf3
Tim Bernhard <tim.bernhard@me.com>
2023-02-01 15:40:19 +0100

    aufgabe1-3.php gemacht

---
+++ Aufgaben/Aufgabe 1/aufgabe1-3.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();    //Neue Session wird gestartet
+
+    $anzahl_aufrufe = 1;
+    if (isset($_SESSION['anzahl_aufrufe'])) {       //Prüfe ob es bereits eine gespeicherte anzahl aufrufe gibt
+        $anzahl_aufrufe = $_SESSION['anzahl_aufrufe'];
+    }
+
+    echo "Die Seite wurde {$anzahl_aufrufe}x aufgerufen.";
+
+    $anzahl_aufrufe++;
+    $_SESSION['anzahl_aufrufe'] = $anzahl_aufrufe; //Muss wieder auf der Variable gespeichert werden
