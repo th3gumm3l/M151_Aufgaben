bbf0a6cc2a0b58798b2730215532a09e3f499bdc
Tim Bernhard <tim.bernhard@me.com>
2023-02-08 14:25:04 +0100

    aufgabe2-2.php done

--- Aufgaben/Aufgabe 2/aufgabe2-2.php
+++ Aufgaben/Aufgabe 2/aufgabe2-2.php
@@ -1,1 +1,23 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "northwind";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br> <br>";
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage() . "<br>";
+}
+
+//Datenausgabe
+echo "<br> <h2>Datenausgabe:</h2>";
+$statement = $conn->prepare("SELECT * FROM customers");
+$statement->execute();
+while($row = $statement->fetch()) {
+    echo $row['first_name']." ".$row['last_name']."<br />";
+}
