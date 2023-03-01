b0207162a9ac11c733fa2cefe1e38d2edce726bc
Tim Bernhard <tim.bernhard@me.com>
2023-03-01 14:00:38 +0100

    weiss nid öbs gad

--- Aufgaben/Aufgabe 2/Aufgabe 2-4/add.php
+++ Aufgaben/Aufgabe 2/Aufgabe 2-4/add.php
@@ -14,16 +14,16 @@ catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage() . "<br>";
 }
 
-$sql = "INSERT INTO Customers(company, last_name, first_name, email_adress) VALUES ()";
+$sql = "INSERT INTO Customers(company, last_name, first_name, email_adress) VALUES (:company, :last_name, :first_name, :email_address)";
 
 $statement = $conn->prepare($sql);
 $statement->execute();
 ?>
 
-<form method="POST" action="?">
-    <input type="text" name="company" placeholder="Firma"><br>
-    <input type="text" name="last_name" placeholder="Nachname"><br>
-    <input type="text" name="first_name" placeholder="Vorname"><br>
-    <input type="email" name="email_address" placeholder="Email"><br>
+<form method="POST" action="add.php">
+    <input type="text" name="company" placeholder="company"><br>
+    <input type="text" name="last_name" placeholder="last_name"><br>
+    <input type="text" name="first_name" placeholder="first_name"><br>
+    <input type="email" name="email_address" placeholder="email_address"><br>
     <button type="submit">Hinzufügen</button>
 </form>
